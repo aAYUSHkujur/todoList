75e086731d031c6145087a44d813bc468aa53a76
aayushk720@gmail.com <aayushk720@gmail.com>
2022-11-25 19:56:49 +0530

    updated

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
 
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+
+const item3 = new Item({
+  name: "<--Hit this button to delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Added saved default items to db!");
+  }
+});
 
 app.set("view engine", "ejs");
 
@@ -13,10 +40,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
- 
-  const day = date.getDate();
   res.render("list", {
-    listTitle: day,
+    listTitle: "Today",
     newListItems: items,
   });
 });
